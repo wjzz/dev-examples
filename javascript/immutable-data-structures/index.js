edd724f414d7959bb3821ba93398b9611cb9ef78
Wojciech Jedynak <w.jedynak@univapay.com>
2019-08-13 11:17:27 +0900

    Some Object.freeze examples

--- javascript/immutable-data-structures/index.js
+++ javascript/immutable-data-structures/index.js
@@ -38,3 +38,53 @@ console.log(list1.toString(), " ==> ", list2.toString());
 
 const list3 = list2.map(number => `element #${number}`);
 console.log(list3.toString());
+
+//==============================================
+// Object.freeze EXAMPLES
+//==============================================
+
+const l = [1, 2, 3, 4, 5];
+l.push(6);
+
+console.log(l, Object.isFrozen(l));
+
+Object.freeze(l);
+console.log(Object.isFrozen(l));
+
+try {
+  l.push(7); // l is frozen, so this throws an error!
+} catch (e) {
+  if (e instanceof TypeError) {
+    console.log("e cannot be changed!");
+    console.error(e);
+  }
+}
+
+// this is OK, concat doesn't modify the object
+const l2 = l.concat(7);
+console.log(l2);
+
+// reading values is OK as well
+console.log(l[3]);
+
+l[3] = 55; // this is silently ignored, to get an error use strict mode
+console.log(l);
+
+// freeze is shallow!
+const o = { a: { w: 123 }, b: 4 };
+Object.freeze(o);
+console.log(o);
+
+// nothing happens here
+o.a = 44;
+o.b = 44;
+console.log(o);
+
+// this changes the inner object
+o.a.w = 44;
+console.log(o);
+
+// to stop this, you have to freeze all the way down
+Object.freeze(o.a);
+o.a.w = 1244;
+console.log(o);
